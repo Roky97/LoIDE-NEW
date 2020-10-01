d92f148448c6fb0d9573e7a9e088fc328f822033
Roky97 <roccopalermiti@virgilio.it>
2020-10-01 19:05:00 +0200

    Add clear output feature

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -14,31 +14,45 @@ const Output: React.FC<OutputProps> = (props) => {
     const outputModel = OutputStore.useState((o) => o.model);
     const outputError = OutputStore.useState((o) => o.error);
 
+    const clearOutput = () => {
+        OutputStore.update((o) => {
+            o.model = "";
+            o.error = "";
+        });
+    };
+
     return (
         <div className="loide-output">
             <div className="output-settings">
                 <span className="output-title"> Output </span>
                 <div>
-                    <Button variant="light" className="btn-sm mr-2">
+                    {/* <Button variant="light" className="btn-sm mr-2">
                         <FontAwesomeIcon icon="download" />
-                        {/* put tooltip */}
-                    </Button>
-                    <Button variant="light" className="btn-sm mr-2">
-                        <FontAwesomeIcon icon="eraser" />
-                        {/* put tooltip */}
-                    </Button>
+                    </Button> */}
                     <Button
                         variant="light"
                         className="btn-sm mr-2"
-                        onClick={props.split}
+                        disabled={
+                            outputModel.length === 0 && outputError.length === 0
+                        }
+                        onClick={clearOutput}
                     >
-                        {props.pos === OutputPositions.right && (
-                            <FontAwesomeIcon icon="chevron-down" />
-                        )}
-                        {props.pos === OutputPositions.bottom && (
-                            <FontAwesomeIcon icon="chevron-up" />
-                        )}
+                        <FontAwesomeIcon icon="eraser" />
                     </Button>
+                    {props.pos !== OutputPositions.disabled && (
+                        <Button
+                            variant="light"
+                            className="btn-sm mr-2"
+                            onClick={props.split}
+                        >
+                            {props.pos === OutputPositions.right && (
+                                <FontAwesomeIcon icon="chevron-down" />
+                            )}
+                            {props.pos === OutputPositions.bottom && (
+                                <FontAwesomeIcon icon="chevron-up" />
+                            )}
+                        </Button>
+                    )}
                 </div>
             </div>
             <div className="output-content">
